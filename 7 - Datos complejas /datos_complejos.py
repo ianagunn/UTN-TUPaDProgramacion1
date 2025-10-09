9c6b5aec16d069b86a5413cc6860bd1e4cbba216
ianagunn <iangunn007@gmail.com>
2025-10-08 21:26:39 -0300

    Práctica 7: Datos complejos

--- 7 - Datos complejas /datos_complejos.py
+++ 7 - Datos complejas /datos_complejos.py
@@ -70,7 +70,106 @@ print("\nCantidad de veces que aparece cada palabra:")
 print(frecuencia)
 
 #Ejercicio 6
+alumnos = {}
+
+for i in range(3):
+    print("\n----- Alumno -----")
+    nombre = input("\nIngrese el nombre del alumno: ")
+
+    print("\n----- Notas -----")
+    notas = []
+    for j in range(3):
+        nota = float(input(f"\nIngrese la nota de {nombre}: "))
+        notas.append(nota)
+    
+    alumnos[nombre] = tuple(notas) 
+
+print("\n----- Promedios -----")
+for nombre in alumnos:
+    notas_alumno = alumnos[nombre]
+    total = 0
+    for nota in notas_alumno:
+        total += nota
+    promedio = total / len(notas_alumno)
+    print(f'\n{nombre}: {promedio}')
+
 #Ejercicio 7
+parcial_1 = {"Ana", "Juan", "Pedro", "Lucía", "Carla"}
+parcial_2 = {"Pedro", "Carla", "María", "Sofía", "Juan"}
+
+print(f'\nLos que aprobaron ambos parciales son {parcial_1&parcial_2}')
+print(f'\nLos que aprobaron un solo parcial son {parcial_1^parcial_2}')
+print(f'\nLos que aprobaron al menos un parcial son {parcial_1|parcial_2}')
+
 #Ejercicio 8
+productos = {'Clavos':100, 'Tornillos': 50, 'Tuercas': 35}
+menu = [
+    '1 - Consultar stock',
+    '2 - Agregar unidades a un producto existente',
+    '3 - Agregar un nuevo producto',
+    '4 - Salir'
+]
+while True:
+    print('\n----- Menu -----')
+    for i in menu:
+        print(i)
+
+    opcion = input('Ingrese la opción que desea realizar: ')
+
+    match opcion:
+        case '1':
+            print('\n----- Consultar stock -----')
+            consulta = input('\nIngrese el nombre del articulo que desea consultar stock: ')
+            if consulta in productos:
+                print(f'\nEl stock de {consulta} es: {productos[consulta]}')
+            else:
+                print('El producto ingresado no existe')
+
+        case '2':
+            print('\n----- Agregar unidades -----')
+            consulta = input('\nIngrese el nombre del producto que desea agregar stock: ')
+            if consulta in productos:
+                agregar = int(input(f'\nIngrese la cantidad que desea agregar para {consulta}: '))
+                productos[consulta] += agregar
+                print(f'\nEl stock de {consulta} es {productos[consulta]}')
+
+        case '3':
+            print('\n----- Agregar nuevo producto -----')
+            consulta = input('\nIngrese el nombre del nuevo producto: ')
+            if not consulta in productos:
+                agregar = int(input(f'\nIngrese el stock para {consulta}: '))
+                productos[consulta] = agregar
+                print(f'\nSe agregó el nuevo producto {consulta} con un stock de {productos[consulta]}')
+            else:
+                print('\nEl producto ingresado ya existe')
+        case '4':
+            print('\nSaliendo..')
+            break
+
 #Ejercicio 9
+agenda = {
+    ("lunes", "10:00"): "Reunión",
+    ("martes", "14:00"): "Clase de inglés",
+    ("viernes", "18:00"): "Gimnasio"
+}
+
+dia = input('Ingrese el día que desea consultar la actividad: ').lower()
+hora = input('Ingrese la hora (HH:MM): ')
+
+clave = (dia, hora)
+if clave in agenda:
+    print(f'El día {dia} a la hora {hora} tenes: {agenda[clave]}')
+else:
+    print('No hay actividad a esa hora')
+
 #Ejercicio 10
+paises = {
+    'Argentina':'Buenos Aires',
+    'Chile':'Santiago'
+}
+capitales = {}
+
+for pais, capital in paises.items():
+    capitales[capital] = pais
+
+print(capitales)
